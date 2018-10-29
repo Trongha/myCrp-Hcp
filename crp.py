9021f2e18606d5a482866efc8b21529c125ba8d3
SolumateFriend <Trongha.hd.98@gmail.com>
2018-10-30 00:38:44 +0700

    encode2

--- crp.py
+++ crp.py
@@ -1,4 +1,4 @@
-#! / usr / bin / python
+# -*- coding: utf-8 -*-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,9 +15,7 @@ def createFolder(directory):
         print ('Error: Creating directory. ' +  directory)
 
 def state_phase(v, start, dim, tau):
-	#v: vecto
-	#dim là số chiều (số phần tử)	
-	#tau là bước nhảy		
+		
 	return [v[start + i*tau] for i in range(0, dim, 1)]
 
 def predict_diagonal(trainSet, testSet, dim=5, tau=2, epsilon=0.7, lambd=3, percent=0.6, titleOfGraph = 'Pretty Girl', figureName = 'GrilLikeYou', pathSaveFigure = None):
@@ -26,52 +24,21 @@ def predict_diagonal(trainSet, testSet, dim=5, tau=2, epsilon=0.7, lambd=3, perc
 	for i in range(len(trainSet)-(dim-1)*tau):
 		vectors_train_1.append(state_phase(trainSet, i, dim, tau)) 
 
-	#tách statephases
 	vectors_test_1 = []
 	for i in range(len(testSet)-(dim-1)*tau):
 		vectors_test_1.append(state_phase(testSet, i, dim, tau))
 
-	#ép kiểu về array
 	vectors_train_1 = np.array(vectors_train_1)
 	vectors_test_1 = np.array(vectors_test_1)
 
-	# print("train.shape: ", vectors_train_1.shape)
-
-	# r_dist là ma trận khoảng cách
-	# cdist là hàm trong numpy
-	# minkowski là cách tính
-	# p là norm
-	# y là train: đánh số từ trên xuống dưới
-	# x là test
 	r_dist = cdist(vectors_train_1, vectors_test_1, 'minkowski', p=1)
 
-	# epsilon = r_dist.min()
-	# print("r_dist:",r_dist)
-	'''
-	print('vectors_train.shape: ', vectors_train_1.shape) #in ra shape
-	print('vectors_test.shape: ', vectors_test_1.shape)
-	print('r_dist.shape: ', r_dist.shape)
-	print('r_dist min', r_dist.min())
-	print('epsilon: ', epsilon)
-	print('r_dist max', r_dist.max())
-	print('lambd: ', lambd)
-	'''
-
-	#r1 là ma trận -2|-1 thay vì ma trận 01 như crp
-	#-2 là false
-	#-1 là true
-	#r_dist < epsilon trả về ma trận 01, 1 tại điểm r_dist < epsilon
-	#ma trận này trừ đi 2 thì thành ma trận -2|-1
 	r1 = np.array((r_dist < epsilon)-2)
 	# r1 = np.array(r1 - 2)
 	
 	len_r1 = int(np.size(r1[0]))
 	high_r1 = int(np.size(r1)/len_r1)
 
-	#x is an array whose each element is a diagonal of the crp matrix
-	
-	#Đoạn này lấy ra những đoạn đường chéo có số lượng predict liền nhau lớn hơn lamdb
-	#Mỗi phần tử của diagonals_have_predict là một mảng chứa đúng 1 đoạn trùng với train tương ứng đúng vị trí
 	diagonals_have_predict = []
 	for index_diagonal in range(-(high_r1 - lambd + 1), len_r1 - lambd + 2, 1):
 		offset = index_diagonal
@@ -90,20 +57,17 @@ def predict_diagonal(trainSet, testSet, dim=5, tau=2, epsilon=0.7, lambd=3, perc
 						predicts[index] = index + offset
 					diagonals_have_predict.append(predicts)
 			y+=1
-	
-	#Mảng quy đổi về index của dữ liệu test từ index của statePhrase
 
 	indexSampleOrigin = diagonals_have_predict
 
 	for i_row in range(len(indexSampleOrigin)):
 		for i_in_1_State in range(len(indexSampleOrigin[i_row])):
 			if (indexSampleOrigin[i_row][i_in_1_State] >= 0):
-				#Chọn số index state cuối cùng
+
 				if ((i_in_1_State == len(indexSampleOrigin[i_row]) - 1) or (indexSampleOrigin[i_row][i_in_1_State+1] < 0)):
 					for j in range(((int(dim*percent))-1)*tau):
 						indexSampleOrigin[i_row] = np.insert(indexSampleOrigin[i_row], i_in_1_State+j+1, indexSampleOrigin[i_row][i_in_1_State] + j + 1)
 
-	# Lấy giá trị từ index của sample trong testSet
 	valueOfSample = []
 
 	for i in range(len(indexSampleOrigin)):
